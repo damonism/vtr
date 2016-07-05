5cba33b1bca00191554d21ce15981be818cb4b2a
Damon Muller <damon@Kimba.local>
2016-07-05 20:23:43 +1000

    First commit - nothing much here yet

---
+++ 2013 results.R
@@ -0,0 +1,5 @@
+# 2013 results for comparison purposes
+
+tcp_cand_type_2013 <- read.csv("http://results.aec.gov.au/17496/Website/Downloads/HouseTcpByCandidateByVoteTypeDownload-17496.csv",
+                               skip=1, sep=",", header=TRUE)
+
